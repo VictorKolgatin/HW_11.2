eb869c6f5a28d19302a5f47ca82042dd0b3058b6
Victor <kolgatin@gmail.com>
2023-05-16 22:13:59 +0300

    add func title_word and dogstring

--- str_fstr_func.py
+++ str_fstr_func.py
@@ -1,4 +1,15 @@
 def up_letter(word):
+    """
+    Все буквы делает заглавными
+    """
     word_up = word.upper()
 
     return word_up
+
+def title_word (word):
+    """
+    делает заглавными первые буквы каждого слова в строке
+    """
+    title_word = word.title()
+
+    return title_word
